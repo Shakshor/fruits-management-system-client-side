7041a545575a81cb1c93f060bd820db4e5d68af4
Shakshor <shakshor584@gmail.com>
2022-05-17 00:30:33 +0600

    login and registration done and require auth done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,15 +1,35 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    // react firebase hooks
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+
+    // checking the user 
+    if (user) {
+        navigate('/home');
+    }
 
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(email, password);
+        // console.log(email, password);
+
+        signInWithEmailAndPassword(email, password);
     }
 
     return (
@@ -19,9 +39,6 @@ const Login = () => {
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -31,9 +48,12 @@ const Login = () => {
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+                <Button variant="primary" className='w-50 mb-3 d-block mx-auto' type="submit">
+                    Login
                 </Button>
+
+
+                <p>Create an Account <Link to='/register' className='text-decoration-none text-danger'>Register Here!</Link></p>
             </Form>
         </div>
     );
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import auth from '../../../firebase.init'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import './Register.css'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    // react firebase hooks
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    // checking the user
+    if (user) {
+        navigate('/home');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        // creating user for first time
+        createUserWithEmailAndPassword(email, password);
+        event.target.reset();
+    }
+
     return (
-        <div>
+        <div className='register-form'>
             <h2>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name='name' placeholder='Enter Name' required />
+                <input type="email" name='email' placeholder='Enter Email Address' required />
+                <input type="password" name='password' placeholder='Enter Password' required />
+
+                <input type="submit" value="Register" />
+            </form>
+
+            <p>Already have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please login</Link></p>
         </div>
     );
 };
