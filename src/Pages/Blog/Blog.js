a5205c976a1f645647a62bad5359077fb9c114b3
Shakshor <shakshor584@gmail.com>
2022-05-21 13:06:53 +0600

    not found page done and blog page starting

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Accordion } from 'react-bootstrap';
 
 const Blog = () => {
     return (
         <div>
-            <h2>Answer the Questions</h2>
+            <h2>Answer the Questions..</h2>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>!!!!!!!!!!!!</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quas fugit molestias, earum repellat quasi itaque voluptatem molestiae unde consectetur!
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>!!!!!!!!!!!!</Accordion.Header>
+                    <Accordion.Body>
+                        dssssssssssssssssss
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>dssssssssssssssss?</Accordion.Header>
+                    <Accordion.Body>
+                        dsffffffffffffffff
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
     );
 };
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './NotFound.css';
+
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1>Page Not Found</h1>
-            <h2>404</h2>
+        <div className='not-found-area'>
+            <div className='not-found-container'>
+                <h2>Page not found</h2>
+                <h1>404</h1>
+                <button className='back-btn' onClick={() => navigate('/')}>Go Back</button>
+            </div>
         </div>
     );
 };
